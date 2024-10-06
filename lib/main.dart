6068fb015ba9ea93c6157e0c550112a851cdbedd
GPplay <gybramdali2002@hotmail.com>
2024-10-06 16:43:56 -0500

    version estable

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:red_snack_gestion/pages/inicio_sesion.dart';
+import 'package:red_snack_gestion/app/pages/inicio_sesion.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Red Snack Gestion',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
