bfd257afd17efc227ecf12f977ecc3864ef77e09
GPplay <gybramdali2002@hotmail.com>
2024-10-13 20:33:58 -0500

    agregando el nuevo app bar

--- lib/app/pages/home.dart
+++ lib/app/pages/home.dart
@@ -1,79 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:red_snack_gestion/app/pages/inventario.dart';
+import 'package:red_snack_gestion/app/pages/chat_page.dart';
+import 'package:red_snack_gestion/app/widget/appbar.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home Red Snack'),
-        backgroundColor: Colors.red,
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.notifications),
-            onPressed: () {
-              // Lógica para notificaciones
-            },
-          ),
-        ],
-      ),
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: <Widget>[
-            const DrawerHeader(
-              decoration: BoxDecoration(
-                color: Colors.red,
-              ),
-              child: Text(
-                'Menú',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 24,
-                ),
-              ),
-            ),
-            ListTile(
-              leading: const Icon(Icons.dashboard),
-              title: const Text('Dashboard'),
-              onTap: () {
-                // Lógica para ir al Dashboard
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              leading: const Icon(Icons.inventory),
-              title: const Text('Inventario'),
-              onTap: () {
-                // Lógica para ir al Inventario
-                  Navigator.push(
-                  context,
-                  // ignore: prefer_const_constructors
-                  MaterialPageRoute(builder: (context) => InventarioScreen()));
-              },
-            ),
-            ListTile(
-              leading: const Icon(Icons.settings),
-              title: const Text('Configuraciones'),
-              onTap: () {
-                // Lógica para ir a Configuraciones
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              leading: const Icon(Icons.exit_to_app),
-              title: const Text('Cerrar Sesión'),
-              onTap: () {
-                // Lógica para cerrar sesión
-                Navigator.pop(context);
-              },
-            ),
-          ],
-        ),
-      ),
-      body: const Center(
+    return const Scaffold(
+      appBar: GlobalAppBar(title: 'Home', chatPage: Chat()), 
+      drawer: SideMenu(),
+      
+      body: Center(
         child: Text(
           'Bienvenido a Red Snack!',
           style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
